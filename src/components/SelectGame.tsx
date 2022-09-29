ed2b733f5f16c0971a4de6e04f4adf3d90025236
Renan Andrade <Fel9.renan02@gmail.com>
2022-09-29 08:49:21 -0300

    Added SelectGame component

---
+++ src/components/SelectGame.tsx
@@ -0,0 +1,34 @@
+import * as Select from "@radix-ui/react-select";
+import { CaretDown } from "phosphor-react";
+import { Games } from "../App";
+
+interface SelectGameProps {
+  games: Games[];
+}
+export function SelectGame({ games }: SelectGameProps) {
+  return (
+    <Select.Root name="game">
+      <Select.Trigger className="flex items-center justify-between bg-zinc-900 py-3 px-4 rounded text-sm placeholder:text-zinc-500">
+        <Select.Value placeholder="Selecione o game" className="text-white" />
+        <Select.Icon>
+          <CaretDown width={20} />
+        </Select.Icon>
+      </Select.Trigger>
+      <Select.Portal>
+        <Select.Content className="bg-zinc-900 py-3 px-4 text-sm text-white">
+          <Select.Viewport className="flex flex-col gap-2">
+            {games?.map((game) => (
+              <Select.Item
+                key={game.id}
+                value={game.id}
+                className="py-1 hover:bg-zinc-800"
+              >
+                <Select.ItemText>{game.name}</Select.ItemText>
+              </Select.Item>
+            ))}
+          </Select.Viewport>
+        </Select.Content>
+      </Select.Portal>
+    </Select.Root>
+  );
+}
