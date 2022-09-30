b2283e2102eb29ed7bbf31404a2737d6abc84835
Renan Andrade <Fel9.renan02@gmail.com>
2022-09-30 15:31:02 -0300

    Altered name and ads count position

--- src/components/GameBanner.tsx
+++ src/components/GameBanner.tsx
@@ -6,13 +6,15 @@ export function GameBanner(props: Games) {
     .replace("{height}", "300");
 
   return (
-    <a href="" className="relative rounded-lg overflow-hidden">
-      <img src={imgUrl} alt="" />
-      <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 right-0 left-0">
-        <strong className="font-bold text-white">{props.name}</strong>
-        <span className="text-sm text-zinc-300 block">
-          {`${props.ads} anúncio(s)`}
-        </span>
+    <a href="" className="overflow-hidden justify-center flex">
+      <div className="relative">
+        <img src={imgUrl} alt="" className="rounded-lg" />
+        <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 right-0 left-0 rounded-b-lg">
+          <strong className="font-bold text-white">{props.name}</strong>
+          <span className="text-sm text-zinc-300 block">
+            {`${props.ads} anúncio(s)`}
+          </span>
+        </div>
       </div>
     </a>
   );
