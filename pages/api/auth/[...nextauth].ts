591bab364c04bf4714bfb5b9136de92902f85da6
Renan Andrade <Fel9.renan02@gmail.com>
2022-11-02 20:13:03 -0300

    Added user information to session in next auth

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,12 +1,58 @@
+import axios from "axios";
 import NextAuth, { NextAuthOptions } from "next-auth";
-import DiscordProvider from "next-auth/providers/discord";
+import DiscordProvider, { DiscordProfile } from "next-auth/providers/discord";
+
 export const authOptions: NextAuthOptions = {
   providers: [
     DiscordProvider({
       clientId: process.env.DISCORD_CLIENT_ID,
       clientSecret: process.env.DISCORD_CLIENT_SECRET,
+      async profile(profile: DiscordProfile) {
+        if (profile.avatar === null) {
+          const defaultAvatarNumber = parseInt(profile.discriminator) % 5;
+          profile.image_url = `https://cdn.discordapp.com/embed/avatars/${defaultAvatarNumber}.png`;
+        } else {
+          const format = profile.avatar.startsWith("a_") ? "gif" : "png";
+          profile.image_url = `https://cdn.discordapp.com/avatars/${profile.id}/${profile.avatar}.${format}`;
+        }
+        try {
+          const user = (
+            await axios.post("http://localhost:8000/users", {
+              id: profile.id,
+              name: profile.username,
+              username: profile.username + "#" + profile.discriminator,
+              email: profile.email,
+              image: profile.image_url,
+            })
+          ).data;
+
+          return user;
+        } catch (error) {
+          console.log(error);
+          return {};
+        }
+      },
     }),
   ],
+  callbacks: {
+    async jwt({ token, user }) {
+      if (user) {
+        const userInfo = {
+          id: user.id,
+          name: user.name,
+          username: user.username,
+          email: user.email,
+          image: user.image,
+        };
+        token.user = userInfo;
+      }
+      return token;
+    },
+    async session({ token, session }) {
+      session.user = token.user;
+      return session;
+    },
+  },
   pages: {
     error: "/",
     signIn: "/",
